1e8e6ac9cb7219df3c1ac460e0a191eb2e9cddea
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-21 18:05:21 +0500

    adding database atlas name in Database connection

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://ilyas:ilyas@cluster0.suvdu4n.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://ilyas:ilyas@cluster0.suvdu4n.mongodb.net/hotelbooking?retryWrites=true&w=majority')
         console.log('MongoDB connected...')
     } catch (err) {
         console.error(err.message);
