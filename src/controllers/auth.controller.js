abafd7288b09acb2dbf74c69e8a277cdf6e7d22e
Ilyas Khan <ilyassanjrani708@gmail.com>
2022-09-28 17:25:06 +0500

    adding loggedIn controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const User = require('../models/User.modal.js')
 const STATUS = require('../config/status')
+const dotenv = require('dotenv');
 const controller = {};
+
 
 controller.register = async (req, res) => {
     try {
@@ -55,6 +57,18 @@ controller.login = async (req, res) => {
         })
     } catch (err) {
         res.send({ message: err.message })
+    }
+}
+
+controller.loggedIn = async (req, res) => {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) return res.json(false)
+        jwt.verify(token, process.env.JWT_SECTRET)
+        res.send(true)
+    } catch (error) {
+        res.json(false)
     }
 }
 
